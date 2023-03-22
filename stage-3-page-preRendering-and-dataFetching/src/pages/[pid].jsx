d0498a4730e7dba0cbcdb9fe8105b06e2ea0784d
evyatar madari <evyatarmadarri@gmail.com>
2023-03-22 21:07:22 +0200

    .

--- stage-3-page-preRendering-and-dataFetching/src/pages/[pid].jsx
+++ stage-3-page-preRendering-and-dataFetching/src/pages/[pid].jsx
@@ -18,33 +18,8 @@ function Product(props)  {
   )
 }
 
-const fetchCountries = async () => {
-    const getCountries = await fetch("https://restcountries.com/v3.1/all");
-    const countries = await getCountries.json();
-    return countries;
-}
-
-export async function getStaticPaths(context) {
-
-    const countries = await fetchCountries();
-    const paths = countries.map((country, index) => ({
-        params: { pid: country.cca3 },
-    }));
-    
-    return {
-        paths: paths,
-        // paths: [
-        //     { params: { pid: "p1" } },
-        // ],
-        fallback: 'blocking' // true: if the path is not found, it will be generated on the run time (client side)
-        // fallback: true, // true: if the path is not found, it will be generated on the run time (client side)
-    }
-}
-
-export async function getStaticProps(context) {
-    // extract the params from the context (in the server)
-    const { params } = context
-    const { pid } = params
+const fetchProducts = async () => {
+    // return obj with products data
     const fs = require("fs/promises");
     const path = require("path");
 
@@ -52,11 +27,45 @@ export async function getStaticProps(context) {
     const filePath = path.join(process.cwd(), "src/data", "dummy-backend.json");
     const jsonData = await fs.readFile(filePath);
     const data = JSON.parse(jsonData);
-    
+    return data.products;
+}
+
+export async function getStaticProps(context) {
+    // extract the params from the context (in the server)
+    const { params } = context
+    const { pid } = params
+
+    const products = await fetchProducts();
+    const product = products.find((product) => product.id === pid);
+
+    if (!product) {
+        return {
+            notFound: true,
+        }
+    }
+
     return {
         props: {
-        product: data.products.find((product) => product.id === pid),
+        product: product,
+        },
+    }
+}
+
+export async function getStaticPaths() {
+
+    const product = await fetchProducts();
+    const paths = product.map((prod, index) => {
+        if (index > 5 ) return;
+        return {
+            params: { pid: prod.id },
         }
+    });
+
+    
+    return {
+        paths: paths,
+        fallback: true // true: if the path is not found, it will be generated on the run time (client side)
+        // fallback: true, // true: if the path is not found, it will be generated on the run time (client side)
     }
 }
 
