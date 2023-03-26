fd10bb68be0e840c932b68b18f94e8b4b4493fb0
evyatar madari <evyatarmadarri@gmail.com>
2023-03-26 18:40:52 +0300

    completed pre-rendering

--- stage-3-page-preRendering-and-dataFetching/src/pages/sales.jsx
+++ stage-3-page-preRendering-and-dataFetching/src/pages/sales.jsx
@@ -5,7 +5,7 @@ const sales = () => {
     const { data, error } = useSWR('https://jsonplaceholder.typicode.com/todos/1');
     // the data will be fetched on the client side.
     // and if the data will changed in the server, the data will be updated on the client side
-    
+
   return (
     <div>sales</div>
   )
