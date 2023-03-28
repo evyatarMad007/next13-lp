4ab9a86f8aff5969782a70c30e6c45a770c54db9
= <=>
2023-03-29 00:04:04 +0300

    dymanic page finished.

--- stage-4-page-preRendering-and-dataFetching-project/src/pages/events/[eventId].jsx
+++ stage-4-page-preRendering-and-dataFetching-project/src/pages/events/[eventId].jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { getEventById, getAllEvents } from "@/helpers/api-util";
+import { getEventById, getFeaturedEvents } from "@/helpers/api-util";
 import EventSummary from "@/components/events/EventDetail/EventSummary/EventSummary";
 import EventLogistics from "@/components/events/EventDetail/EventLogistics/EventLogistics";
 import EventContent from "@/components/events/EventDetail/EventContent/EventContent";
@@ -13,30 +13,29 @@ import Button from "@/components/ui/button/Button";
 
 // @ page pre-rendering
 // Goal: we want to pre-render the page at build time
-// Rendering type: SSG (Static Site Generation) + Dynamic Routing. 
+// Rendering type: SSG (Static Site Generation) + Dynamic Routing.
 // technique: getStaticProps (function) + getStaticPaths (function) + fallback (true) - because we don't know the number of events.
 const EventDetailPage = (props) => {
   const event = props.selectedEvent;
-
+  
+ 
   if (!event) {
-    return <>
-    <ErrorAlert>
-          <p>No event found!</p>
-        </ErrorAlert>
+    return (
         <div className="center">
-          <Button link="/events">Show All Events</Button>
+          <p>Loading...</p>
         </div>
-    </>;
+    );
   }
+
+
   return (
     <div>
       <EventSummary title={event.title} />
-      <EventLogistics 
-      date={event.date}
-      address={event.location}
-      image={event.image}
-      imageAlt={event.title}
-      
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
       />
       <EventContent>
         <p>{event.description}</p>
@@ -48,6 +47,13 @@ const EventDetailPage = (props) => {
 export async function getStaticProps(context) {
   const eventId = context.params.eventId;
   const event = await getEventById(eventId);
+  console.log("🚀 ~ file: [eventId].jsx:50 ~ getStaticProps ~ event:", event)
+  
+  if (!event) {
+    return {
+      notFound: true,
+    };
+  }
 
   return {
     props: {
@@ -58,12 +64,14 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
-  const events = await getAllEvents();
+  const events = await getFeaturedEvents();
   const paths = events.map((event) => ({ params: { eventId: event.id } }));
 
   return {
     paths: paths,
-    fallback: false, // false: if the path is not found, it will return 404 page. true: if the path is not found, it will return the fallback page.
+    // fallback: false, // false: if the path is not found in the paths array, it will return 404 page.
+    fallback: true, // true: if the path is not found in the paths array, it will return the fallback page. But it will not wait until the page is generated.
+    // fallback: "blocking", // blocking: if the path is not found in the paths array, it will return the fallback page. But it will wait until the page is generated.
   };
 }
 
