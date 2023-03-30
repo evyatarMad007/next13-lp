4631c8226b7e17317e03494b391ffd629e04dcc8
= <=>
2023-03-30 23:34:55 +0300

    creating a one api endpoint that save from front fedback to JSON file.

--- stage-6-backend-code-with-api-routes-fs-react/src/pages/api/feedback.js
+++ stage-6-backend-code-with-api-routes-fs-react/src/pages/api/feedback.js
@@ -1,5 +1,31 @@
+import fs from 'fs';
+import path from 'path';
+
 function handler(req, res) {
-    res.status(200).json({ feedback: 'This is feedback from the backend'})
+
+    if (req.method === 'POST') {
+        const email = req.body.email;
+        const feedbackText = req.body.text;
+
+        const newFeedback = {
+            id: new Date().toISOString(),
+            email: email,
+            text: feedbackText
+        };
+
+        console.log(newFeedback);
+
+        // store that in a database or in a file
+        const filePath = path.join(process.cwd(), 'src/data', 'feedback.json');
+        const fileData = fs.readFileSync(filePath);
+        const data = JSON.parse(fileData);
+        data.push(newFeedback);
+        fs.writeFileSync(filePath, JSON.stringify(data));
+
+        res.status(201).json({ message: 'Success!', feedback: newFeedback });
+    } else {
+        res.status(200).json({ message: 'This is feedback from the backend' });
+    }
 }
 
 export default handler;
--- stage-6-backend-code-with-api-routes-fs-react/src/pages/index.jsx
+++ stage-6-backend-code-with-api-routes-fs-react/src/pages/index.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, {useRef} from 'react'
 
 const HomePage = () => {
+
+  const emailInputRef = useRef();
+  const messageInputRef = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredMessage = messageInputRef.current.value;
+
+    const reqBody = {
+      email: enteredEmail,
+      text: enteredMessage
+    };
+
+
+    fetch('/api/feedback', {
+      method: 'POST',
+      body: JSON.stringify(reqBody),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <br />
+          <input type="email" id="email" ref={emailInputRef}/>
+        </div>
+
+        <div>
+          <label htmlFor="message">Your Message</label>
+          <br />
+          <textarea id="message" rows="5" ref={messageInputRef}></textarea>
+        </div>
+
+        <button>Send Feedback</button>
+      </form>
     </div>
   )
 }
