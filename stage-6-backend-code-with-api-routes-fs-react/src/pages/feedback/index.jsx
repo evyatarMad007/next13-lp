d2f5d84209c265c6a011f15485376920b3b01c93
= <=>
2023-03-31 01:20:28 +0300

    dynamic api route and using him in the component

--- stage-6-backend-code-with-api-routes-fs-react/src/pages/feedback/index.jsx
+++ stage-6-backend-code-with-api-routes-fs-react/src/pages/feedback/index.jsx
@@ -1,18 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { buildFeedbackPath, extractFeedback } from '../api/feedback'
 
 const FeedbackPage = (props) => {
   const { feedbackItems } = props;
+  const [selectedFeedback, setSelectedFeedback] = useState({})
+
+  function loadFeedbackHandler(id) {
+    fetch(`api/${id}`)
+    .then((res) => res.json())
+    .then((data) => setSelectedFeedback(data.feedback))
+  }
+
+
   return (
     <div>
     <h1>FeedbackPage</h1>
-    <br /><br />
+    <br />
+    {selectedFeedback && (
+        <div key={selectedFeedback.id}>
+        <h2>Selected Feedback</h2>
+        <p>{selectedFeedback.email}</p>
+        </div>
+    )}
+    <br /><br /><br /><br />
     <ul>
     {feedbackItems.map((item) => (
         <li key={item.id}>
         <span>Email: {item.email}</span>
         <br />
         <span>Message: {item.text}</span>
+        <br />
+        {/* <button onClick={() => loadFeedbackHandler(item.id)}>Show Details</button> */}
+        <button onClick={loadFeedbackHandler.bind(null,item.id)}>Show Details</button>
         <br /><br />
         <hr />
         <br /><br />
